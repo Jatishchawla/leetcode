db3a77fbd0a7759dca8111cda97a3f5662939414
Jatish chawla <104876153+Jatishchawla@users.noreply.github.com>
2026-05-15 15:36:45 +0530

    Time: 4 ms (0.16%), Space: 9.3 MB (80.26%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int row =matrix.size();
+        int col =matrix[0].size();
+        vector<int> out;
+        
+        int top = 0; // row from top
+        int right = col-1; // column from right
+        int bottom = row-1; // row from bottom
+        int left = 0; // column from left 
+
+        cout<<"t "<<"r "<<"b "<<"l "<<endl;
+        cout<<top<<" "<<right <<" "<<bottom<<" "<<left<<endl;
+        while( top<=bottom || right>=left ){
+            // printing top row -> left to right
+            if(top<=bottom){
+                for(int j = left; j<=right ; j++){
+                    
+                    out.push_back(matrix[top][j]);
+                }
+                top++; // next top 
+            }
+            // printing right column -> top to bottom
+            if(left<=right){
+                for(int i = top ; i<=bottom  ; i++ ){
+                    out.push_back(matrix[i][right]);
+                }
+                right --; // next right
+            }
+            // printing bottom row - right to left
+            if(top<=bottom){
+                for(int j = right ; j >= left ; j-- ){
+                    out.push_back(matrix[bottom][j]);
+                }
+                bottom --; // for next bottom
+            }
+            // printing left column - bottom to top
+            if(left<=right){
+                for(int i = bottom ; i >=top ; i--){
+                    out.push_back(matrix[i][left]);
+                }
+                left ++;
+            }
+            cout<<top<<" "<<right <<" "<<bottom<<" "<<left<<endl;
+        }    
+        return out;
+    }
+};
