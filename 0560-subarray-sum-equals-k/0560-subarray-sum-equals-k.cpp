8d680c9157cf18a903e4c9008a71b0ba994f1a82
Jatish chawla <104876153+Jatishchawla@users.noreply.github.com>
2026-05-21 10:45:46 +0530

    Time: 689 ms (19.91%), Space: 45.5 MB (55.47%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mpp; //presum map
+        // int prev=0;
+        int ssf=0 ; // subarray sum so far 
+        int count=0; // number of subarrays with sum k
+        // if(k==0){count++;}
+        mpp[0]=1;
+        for(int i=0; i<n ; i++){
+            ssf+=nums[i];
+            // if(mpp.find(ssf) == mpp.end()){
+            //     mpp[ssf]=0;
+            // }
+            if(mpp.find(ssf-k) != mpp.end()){
+                count+=mpp[ssf-k];
+            }
+            mpp[ssf]++;
+        }
+        for(auto it : mpp){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        return count;
+    }
+};
