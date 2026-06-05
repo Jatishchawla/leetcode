453f2b66ca606ed1e7f838b4d868add928c4f2e9
Jatish chawla <104876153+Jatishchawla@users.noreply.github.com>
2026-06-05 19:01:30 +0530

    0229-majority-element-ii 06-05-2026 Medium 8 ms (29.57%) 23.8 MB (44.52%)

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mpp;
+        // unordered_map<int,int> mpp;
+        sort(nums.begin(),nums.end());
+
         vector<int> majors;
-        for(int i:nums){
-            mpp[i]++;
-            
-        }
+        majors.push_back(INT_MIN);
+        int curr = nums[0]; // current element
+        int curr_count=0; // to store current's occurece count
         int n = nums.size()/3;
-        for(auto it: mpp){
-            if(it.second> n){
-                majors.push_back(it.first);
+        for(int i : nums){
+            if(curr == i){
+                curr_count++; 
+                // cout<<curr<<" "<<curr_count<<endl;            
+            }
+            else{
+                curr=i;
+                curr_count=1;
+
+            }
+            if(curr_count > n && curr != majors.back() ){            
+                majors.push_back(curr);
             }
         }
+        majors.erase(majors.begin() + 0);
+        // for(auto it: mpp){
+        //     if(it.second> n){
+        //         majors.push_back(it.first);
+        //     }
+        // }
+
         return majors;
     }
 };
