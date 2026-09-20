37a477c6bba2b66b5977eb0b72a74bd6d1b10fad
Jatish chawla <104876153+Jatishchawla@users.noreply.github.com>
2026-09-20 21:03:15 +0530

    0073-set-matrix-zeroes 09-20-2026 Medium 0 ms (100%) 21 MB (57.68%)

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+        int m = matrix.size(); // no. of rows
+        int n = matrix[0].size(); // no. of columns
+        int col0 = matrix[0][0]; 
+        for(int i = 0 ; i < m ; i++ ){
+            for(int j=0; j<n;j++){
+                if(matrix[i][j] == 0 ){
+                    matrix[i][0]=0; // marking rows that will set to zero 
+                    if(j==0){
+                        col0=0;
+                    }
+                    else{
+                        matrix[0][j]=0;
+                    }
+                }
+            }
+        }
+        for(int i=1; i<m ; i++){ // i = 1 because 1st row and 1st column column are inter dependent , we will handle them seperately
+            for(int j=1; j<n ; j++){
+                if(matrix[i][j] != 0 ){
+                    // if row or column anyone is marked for this ele. set ele = 0 
+                    if(matrix[0][j] ==0 || matrix[i][0]==0 ){
+                        matrix[i][j]=0;
+                    }
+                }    
 
-        int col0=1;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			if(matrix[i][j]==0){
-				matrix[i][0]=0;
-				if(j!=0){
-					matrix[0][j]=0;
-				}
-				else{
-					col0=0;
-				}
-			}
-		}
-	}
-
-
-	for(int i=1;i<n;i++){
-		for(int j=1;j<m;j++){
-			if(matrix[i][j]!=0){
-				if(matrix[i][0]==0 || matrix[0][j]==0){
-					matrix[i][j]=0;
-				}
-			}
-		}
-	}
-
-
-	if(matrix[0][0]==0){
-		for(int j=0;j<m;j++){
-			matrix[0][j]=0;
-		}
-	}
-	if(col0==0){
-		for(int i=0;i<n;i++){
-			matrix[i][0]=0;
-		}
-	}
+            }
+        }
+        // handling 1st column 
+        if(matrix[0][0]==0){
+            for(int j=0 ; j<n ; j++ ){
+                matrix[0][j] = 0;
+            }
+        }
+        // handling 1st row
+        if(col0 == 0){
+            for(int i=0 ; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
